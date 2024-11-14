101aaa024d295086644da33054ef2dedf1e985d1
mythilipriya <mythilipriya.e@diggibyte.com>
2024-11-14 17:20:16 +0530

    Sales  File

---
+++ src_assignments/bronze_to_silver/sales.py
@@ -0,0 +1,18 @@
+
+raw_sales_data_df = spark.read.csv('dbfs:/mnt/Bronze/sales_view/sales/20240107_sales_data.csv', header=True, inferSchema=True)
+
+
+
+sales_data_snake_case_df = toSnakeCase(raw_sales_data_df)
+
+
+
+
+sales_output_location = 'dbfs:/mnt/silver/sales_view/customer_sales'
+
+
+write_delta_upsert(sales_data_snake_case_df, sales_output_location)
+
+
+
+
